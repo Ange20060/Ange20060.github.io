10010721500954c6a990201faf41be944f44d552
Melris ZOHOUN <zohounmelris@gmail.com>
2026-02-01 11:25:35 +0100

    correctin et mise a jour

--- asset/script.js
+++ asset/script.js
@@ -1,12 +1,12 @@
 // thème jour et nuit
-const themeToggle = document.getElementById('theme-toggle');
+const themeToggle = document.getElementById('themeToggle');
 themeToggle.addEventListener('click', () => {
     document.documentElement.classList.toggle('dark');
 });
 // commande whatsapp
-function oderWhatsApp(modelName) {
+function orderWhatsApp(modelName) {
     const phoneNumber = '22946617514'; // Remplacez par le numéro de téléphone souhaité
     const message = `Bonjour, je souhaite commander le modèle : ${modelName}`;
-    const url = `https://wa.me/${22946617514}?text=${encodeURIComponent(message)}`;
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
 }
